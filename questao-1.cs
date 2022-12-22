9a4bca1d522fb6b481a57bf10e559c30ddc94c7a
Bruno melo <65568481+cbrunomelo@users.noreply.github.com>
2022-12-21 22:26:05 -0300

    Update questao-1.cs

--- questao-1.cs
+++ questao-1.cs
@@ -1,6 +1,6 @@
 "Em, C# 10, const strings podem ser inicializadas usando interpolação de string, 
 se todas as strings interpoladas forem também constantes. A interpolação de strings pode 
 criar strings constantes mais legíveis conforme você as produza e faça uso em sua aplicação. 
-Os valores passados para interpolação de string contante não podem conter números contantes, pois 
+Os valores passados para interpolação de string constante não podem conter números contantes, pois 
 os valores dessas contantes numéricas são convertidas para string em tempo de execução. A cultura
 em uso da aplicação pode afetar a representação da string."
